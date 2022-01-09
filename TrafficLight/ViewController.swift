d8528fbb1171bdffd22b485cb89184d3719dbe35
KremnevViacheslav <rusted2192@gmail.com>
2022-01-09 17:47:23 +0300

    Setup view and storyboard

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startLightButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 60
+        yellowLightView.layer.cornerRadius = 60
+        greenLightView.layer.cornerRadius = 60
+        startLightButton.layer.cornerRadius = 10
+        
+        redLightView.alpha = 0.1
+        yellowLightView.alpha = 0.1
+        greenLightView.alpha = 0.1
     }
+    
+    @IBAction func startButtonSwitch(_ sender: UIButton) {
+        sender.tag += 1
 
-
+        if sender.tag == 1 {
+            greenLightView.alpha = 0.1
+            redLightView.alpha = 1
+            startLightButton.setTitle("NEXT", for: .normal)
+        } else if sender.tag == 2 {
+            yellowLightView.alpha = 1
+            redLightView.alpha = 0.1
+        } else if sender.tag == 3 {
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.1
+            sender.tag = 0
+        }
+    }
 }
 
